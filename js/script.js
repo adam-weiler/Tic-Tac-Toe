3f7c502fc48056c01d01d781156fd19a1d84ecb9
adam-weiler <adam.weiler@yahoo.ca>
2019-08-20 16:35:09 -0400

    Declaring all win conditons.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     let playerTurn = 'X';
-    // const btn1 = document.querySelector('button.btn1');
-    // const btn2 = document.querySelector('button.btn2');
-    // const btn3 = document.querySelector('button.btn3');
-    // const btn4 = document.querySelector('button.btn4');
-    // const btn5 = document.querySelector('button.btn5');
-    // const btn6 = document.querySelector('button.btn6');
-    // const btn7 = document.querySelector('button.btn7');
-    // const btn8 = document.querySelector('button.btn8');
-    // const btn9 = document.querySelector('button.btn9');
+    let currentTurn = 1;
+    const btn1 = document.querySelector('input#btn1');
+    const btn2 = document.querySelector('input#btn2');
+    const btn3 = document.querySelector('input#btn3');
+    const btn4 = document.querySelector('input#btn4');
+    const btn5 = document.querySelector('input#btn5');
+    const btn6 = document.querySelector('input#btn6');
+    const btn7 = document.querySelector('input#btn7');
+    const btn8 = document.querySelector('input#btn8');
+    const btn9 = document.querySelector('input#btn9');
 
     const container = document.querySelector('#container');
 
-
-    const allButtons = document.querySelectorAll('button');
-    console.log(allButtons)
+    const allInputs = document.querySelectorAll('input');
+    console.log(allInputs)
 
     console.log(container)
 
@@ -32,6 +32,37 @@ document.addEventListener('DOMContentLoaded', function() {
             // console.log('Remove this!')
             // e.target.remove();
 
+            console.log(currentTurn);
+
+            if (currentTurn >= 5) { //Player cannot win before round 5.
+                console.log(`check for winner ${playerTurn}`);
+
+                if ((btn1.value == playerTurn) && (btn2.value == playerTurn) && (btn3.value == playerTurn)) { //Horizontal
+                    alert('winner!')
+                    //Disable all buttons
+                } else if ((btn4.value == playerTurn) && (btn5.value == playerTurn) && (btn6.value == playerTurn)) { //Horizontal
+                    alert('winner!')
+                    //Disable all buttons
+                } else if ((btn7.value == playerTurn) && (btn8.value == playerTurn) && (btn9.value == playerTurn)) { //Horizontal
+                    alert('winner!')
+                    //Disable all buttons
+                } else if ((btn1.value == playerTurn) && (btn4.value == playerTurn) && (btn7.value == playerTurn)) { //Vertical
+                    alert('winner!')
+                    //Disable all buttons
+                } else if ((btn2.value == playerTurn) && (btn5.value == playerTurn) && (btn8.value == playerTurn)) { //Vertical
+                    alert('winner!')
+                    //Disable all buttons
+                } else if ((btn3.value == playerTurn) && (btn6.value == playerTurn) && (btn9.value == playerTurn)) { //Vertical
+                    alert('winner!')
+                    //Disable all buttons
+                } else if ((btn1.value == playerTurn) && (btn5.value == playerTurn) && (btn9.value == playerTurn)) { //Diagonal
+                    alert('winner!')
+                    //Disable all buttons
+                } else if ((btn3.value == playerTurn) && (btn5.value == playerTurn) && (btn7.value == playerTurn)) { //Diagonal
+                    alert('winner!')
+                    //Disable all buttons
+                }
+            }
 
             if (playerTurn == 'X') {
                 playerTurn = 'O';
@@ -39,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 playerTurn = 'X';
             }
 
+            currentTurn += 1;
         }
-    })
-
-})
+    });
+});
